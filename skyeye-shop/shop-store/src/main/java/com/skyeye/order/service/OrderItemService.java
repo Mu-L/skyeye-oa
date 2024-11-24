a9e1ba17f174e993942ecfb84a3fcbfd0c19a05d
wst <2450473462@qq.com>
2024-11-24 10:43:57 +0800

    feat: 移除多余的包和方法

--- skyeye-shop/shop-store/src/main/java/com/skyeye/order/service/OrderItemService.java
+++ skyeye-shop/shop-store/src/main/java/com/skyeye/order/service/OrderItemService.java
@@ -1,10 +1,8 @@
 package com.skyeye.order.service;
 
 import com.skyeye.base.business.service.SkyeyeBusinessService;
-import com.skyeye.common.enumeration.WhetherEnum;
 import com.skyeye.order.entity.Order;
 import com.skyeye.order.entity.OrderItem;
-import org.aspectj.weaver.ast.Or;
 
 import java.util.List;
 import java.util.Map;
@@ -13,8 +11,6 @@ public interface OrderItemService extends SkyeyeBusinessService<OrderItem> {
 
 
     void deleteByPerentIds(List<String> ids);
-
-//    List<OrderItem> selectByParentId(String id);
 
     List<OrderItem> queryListByStateAndOrderId(String orderId, Integer state);
 
