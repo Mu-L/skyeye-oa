6d6bfac464219824a071fee637875d8667c5453f
weizhiqiang <598748873@qq.com>
2024-12-31 20:43:03 +0800

    feat: 租户管理重构为多租户

--- skyeye-promote/skyeye-userauth/src/main/java/com/skyeye/tenant/service/impl/TenantServiceImpl.java
+++ skyeye-promote/skyeye-userauth/src/main/java/com/skyeye/tenant/service/impl/TenantServiceImpl.java
@@ -12,6 +12,7 @@ import com.skyeye.annotation.service.SkyeyeService;
 import com.skyeye.base.business.service.impl.SkyeyeBusinessServiceImpl;
 import com.skyeye.common.constans.CommonConstants;
 import com.skyeye.common.constans.CommonNumConstants;
+import com.skyeye.common.enumeration.TenantEnum;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
 import com.skyeye.exception.CustomException;
@@ -38,7 +39,7 @@ import java.util.stream.Collectors;
  * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
  */
 @Service
-@SkyeyeService(name = "租户管理", groupName = "租户管理")
+@SkyeyeService(name = "租户管理", groupName = "租户管理", tenant = TenantEnum.PLATE)
 public class TenantServiceImpl extends SkyeyeBusinessServiceImpl<TenantDao, Tenant> implements TenantService {
 
     @Autowired
