545eed7d5818d9075f1849ef339ff498936d3bef
wst <2450473462@qq.com>
2025-01-10 16:25:31 +0800

    feat: 修改学校、院系等信息为非必填，tag为必填

--- skyeye-school/school-exam/src/main/java/com/skyeye/eve/examquestion/entity/Question.java
+++ skyeye-school/school-exam/src/main/java/com/skyeye/eve/examquestion/entity/Question.java
@@ -109,7 +109,7 @@ public class Question extends OperatorUserInfo {
     private Integer randOrder;
 
     @TableField("tag")
-    @ApiModelProperty(value = "标记 1题库中的题   2试卷中的题")
+    @ApiModelProperty(value = "标记 1题库中的题   2试卷中的题",required = "required")
     private Integer tag;
 
     @TableField("visibility")
@@ -149,19 +149,19 @@ public class Question extends OperatorUserInfo {
     private Integer isDelete;
 
     @TableField(value = "school_id")
-    @ApiModelProperty(value = "学校id",required = "required")
+    @ApiModelProperty(value = "学校id")
     private String schoolId;
 
     @TableField(value = "faculty_id")
-    @ApiModelProperty(value = "院系id",required = "required")
+    @ApiModelProperty(value = "院系id")
     private String facultyId;
 
     @TableField(value = "major_id")
-    @ApiModelProperty(value = "专业id",required = "required")
+    @ApiModelProperty(value = "专业id")
     private String majorId;
 
     @TableField(value = "subject_id")
-    @ApiModelProperty(value = "科目id",required = "required")
+    @ApiModelProperty(value = "科目id")
     private String subjectId;
 
     @TableField(exist = false)
