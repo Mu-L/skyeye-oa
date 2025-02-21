2db027a3a999f3c6498ba9618e2ab75971cb14e4
luluyuyu <3305167204@qq.com>
2025-02-21 09:36:51 +0800

    feat:documentVersion

---
+++ skyeye-promote/skyeye-common/src/main/java/com/skyeye/office/dao/DocumentVersionDao.java
@@ -0,0 +1,7 @@
+package com.skyeye.office.dao;
+
+import com.skyeye.eve.dao.SkyeyeBaseMapper;
+import com.skyeye.office.entity.DocumentVersion;
+
+public interface DocumentVersionDao extends SkyeyeBaseMapper<DocumentVersion> {
+}
