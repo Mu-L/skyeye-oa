e06a1428d03f117a85f184761aa5e6cf8a8d2f47
luluyuyu <3305167204@qq.com>
2025-02-21 09:13:04 +0800

    feat:office评论

--- skyeye-promote/skyeye-common/src/main/java/com/skyeye/office/service/impl/DocumentCommentServiceImpl.java
+++ skyeye-promote/skyeye-common/src/main/java/com/skyeye/office/service/impl/DocumentCommentServiceImpl.java
@@ -1,10 +1,13 @@
 package com.skyeye.office.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.skyeye.base.business.service.impl.SkyeyeBusinessServiceImpl;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
 import com.skyeye.common.util.ToolUtil;
+import com.skyeye.common.util.mybatisplus.MybatisPlusUtil;
+import com.skyeye.office.dao.DocumentCommentDao;
 import com.skyeye.office.entity.DocumentComment;
 import com.skyeye.office.service.DocumentCommentService;
 import org.springframework.stereotype.Service;
@@ -21,80 +24,74 @@ import java.util.stream.Collectors;
  * @date: 2024/1/10
  */
 @Service
-public class DocumentCommentServiceImpl extends SkyeyeBusinessServiceImpl<DocumentComment> implements DocumentCommentService {
+public class DocumentCommentServiceImpl extends SkyeyeBusinessServiceImpl<DocumentCommentDao,DocumentComment> implements DocumentCommentService {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void addComment(InputObject inputObject, OutputObject outputObject) {
-        String documentId = inputObject.getParams().getString("documentId");
-        String content = inputObject.getParams().getString("content");
-        String parentId = inputObject.getParams().getString("parentId");
-
+        Map<String, Object> map = inputObject.getParams();
+        String documentId = map.get("documentId").toString();
+        String content = map.get("content").toString();
+        String parentId = map.get("parentId").toString();
         DocumentComment comment = new DocumentComment();
         comment.setDocumentId(documentId);
         comment.setContent(content);
         comment.setParentId(parentId);
-
-        super.createEntity(comment);
+        super.createEntity(comment, StrUtil.EMPTY);
         outputObject.setBean(comment);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void deleteComment(InputObject inputObject, OutputObject outputObject) {
-        String id = inputObject.getParams().getString("id");
-
+        String id = inputObject.getParams().get("id").toString();
+        QueryWrapper<DocumentComment> queryWrapper = new QueryWrapper<>();
         // 删除评论及其所有回复
-        super.deleteByWrapper(ToolUtil.getWrapper(DocumentComment.class)
-            .eq("id", id)
-            .or()
-            .eq("parent_id", id));
+        queryWrapper.eq(MybatisPlusUtil.toColumns(DocumentComment::getDocumentId), id)
+                .or()
+                .eq(MybatisPlusUtil.toColumns(DocumentComment::getParentId), id);
+        remove(queryWrapper);
     }
 
     @Override
     public void getCommentList(InputObject inputObject, OutputObject outputObject) {
-        String documentId = inputObject.getParams().getString("documentId");
-
+        String documentId = inputObject.getParams().get("documentId").toString();
+        QueryWrapper<DocumentComment> queryWrapper = new QueryWrapper<>();
         // 查询主评论（没有父评论的评论）
-        List<DocumentComment> comments = super.selectList(ToolUtil.getWrapper(DocumentComment.class)
-            .eq("document_id", documentId)
-            .isNull("parent_id")
-            .orderByDesc("create_time"));
-
+        queryWrapper.eq(MybatisPlusUtil.toColumns(DocumentComment::getDocumentId), documentId)
+                .isNull(MybatisPlusUtil.toColumns(DocumentComment::getParentId))
+                .orderByDesc(MybatisPlusUtil.toColumns(DocumentComment::getCreateTime));
+        List<DocumentComment> comments = list(queryWrapper);
         // 获取每个评论的回复数量
         if (!comments.isEmpty()) {
             List<String> commentIds = comments.stream()
-                .map(DocumentComment::getId)
-                .collect(Collectors.toList());
-
+                    .map(DocumentComment::getDocumentId)
+                    .collect(Collectors.toList());
             QueryWrapper<DocumentComment> replyWrapper = new QueryWrapper<>();
-            replyWrapper.select("parent_id, count(1) as reply_count")
-                .in("parent_id", commentIds)
-                .groupBy("parent_id");
-
-            List<Map<String, Object>> replyCounts = super.selectMaps(replyWrapper);
+            replyWrapper.select(MybatisPlusUtil.toColumns(DocumentComment::getParentId),
+                            "count(1) as reply_count")
+                    .in(MybatisPlusUtil.toColumns(DocumentComment::getParentId), commentIds)
+                    .groupBy(MybatisPlusUtil.toColumns(DocumentComment::getParentId));
+            List<Map<String, Object>> replyCounts = super.baseMapper.selectMaps(replyWrapper);
             Map<String, Integer> replyCountMap = replyCounts.stream()
-                .collect(Collectors.toMap(
-                    map -> map.get("parent_id").toString(),
-                    map -> Integer.valueOf(map.get("reply_count").toString())
-                ));
-
-            comments.forEach(comment -> 
-                comment.setReplyCount(replyCountMap.getOrDefault(comment.getId(), 0)));
+                    .collect(Collectors.toMap(
+                            map -> map.get("parent_id").toString(),
+                            map -> Integer.valueOf(map.get("reply_count").toString())
+                    ));
+            comments.forEach(comment ->
+                    comment.setReplyCount(replyCountMap.getOrDefault(comment.getDocumentId(), 0)));
         }
-
         outputObject.setBean(comments);
     }
 
     @Override
     public void getReplyList(InputObject inputObject, OutputObject outputObject) {
-        String parentId = inputObject.getParams().getString("parentId");
-
+        String parentId = inputObject.getParams().get("parentId").toString();
         // 查询指定评论的所有回复
-        List<DocumentComment> replies = super.selectList(ToolUtil.getWrapper(DocumentComment.class)
-            .eq("parent_id", parentId)
-            .orderByAsc("create_time"));
-
+        QueryWrapper<DocumentComment> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq(MybatisPlusUtil.toColumns(DocumentComment::getParentId), parentId)
+                .orderByAsc(MybatisPlusUtil.toColumns(DocumentComment::getCreateTime));
+        List<DocumentComment> replies = list(queryWrapper);
         outputObject.setBean(replies);
     }
 } 
