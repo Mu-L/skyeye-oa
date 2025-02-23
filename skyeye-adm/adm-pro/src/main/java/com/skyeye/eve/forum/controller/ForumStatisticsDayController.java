963798a48eaaf2aa61c9c4cc76dfc02d52f22c9a
lqy <3431632001@qq,com>
2025-02-23 21:39:14 +0800

    feat:重构adm论坛通知管理

---
+++ skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/controller/ForumStatisticsDayController.java
@@ -0,0 +1,19 @@
+package com.skyeye.eve.forum.controller;
+
+import com.skyeye.annotation.api.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: ForumStatisticsDayController
+ * @Description: 论坛贴子每日的统计表管理控制层
+ * @author: skyeye云系列--卫志强
+ * @date: 2021/7/24 11:48
+ * @Copyright: 2021 https://gitee.com/doc_wei01/skyeye Inc. All rights reserved.
+ * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
+ */
+
+@RestController
+@Api(value = "论坛贴子每日的统计表管理", tags = "论坛贴子每日的统计表管理", modelName = "论坛贴子每日的统计表管理")
+public class ForumStatisticsDayController {
+
+}
---
+++ skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/service/ForumReportHiService.java
@@ -0,0 +1,16 @@
+package com.skyeye.eve.forum.service;
+
+import com.skyeye.base.business.service.SkyeyeBusinessService;
+import com.skyeye.eve.forum.entity.ForumReportHi;
+
+
+/**
+ * @ClassName: ForumReportHiService
+ * @Description: 论坛举报历史表
+ * @author: skyeye云系列--卫志强
+ * @date: 2022/8/9 9:22
+ * @Copyright: 2022 https://gitee.com/doc_wei01/skyeye Inc. All rights reserved.
+ * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
+ */
+public interface ForumReportHiService extends SkyeyeBusinessService<ForumReportHi> {
+}
