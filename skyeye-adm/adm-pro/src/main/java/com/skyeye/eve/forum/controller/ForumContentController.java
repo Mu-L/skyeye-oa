8922b2665da2e35e41e04734bf6b9cd3cbd1a9b5
wst <2450473462@qq.com>
2025-02-25 22:55:08 +0800

    feat:重构adm，删除多余注释

--- skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/controller/ForumContentController.java
+++ skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/controller/ForumContentController.java
@@ -11,7 +11,6 @@ import com.skyeye.annotation.api.ApiOperation;
 import com.skyeye.common.entity.search.CommonPageInfo;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
-import com.skyeye.eve.folder.entity.Folder;
 import com.skyeye.eve.forum.entity.ForumContent;
 import com.skyeye.eve.forum.service.ForumContentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,28 +65,6 @@ public class ForumContentController {
     }
 
     /**
-     * 查询帖子信息用以编辑
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/queryForumContentMationById")
-    public void queryForumContentMationById(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryForumContentMationById(inputObject, outputObject);
-    }
-
-//    /**
-//     * 编辑帖子信息
-//     *
-//     * @param inputObject  入参以及用户信息等获取对象
-//     * @param outputObject 出参以及提示信息的返回值对象
-//     */
-//    @RequestMapping("/post/ForumContentController/editForumContentMationById")
-//    public void editForumContentMationById(InputObject inputObject, OutputObject outputObject) {
-//        forumContentService.editForumContentMationById(inputObject, outputObject);
-//    }
-
-    /**
      * 帖子详情
      *
      * @param inputObject  入参以及用户信息等获取对象
@@ -101,74 +78,21 @@ public class ForumContentController {
         forumContentService.selectById(inputObject, outputObject);
     }
 
-//    @RequestMapping("/post/ForumContentController/queryForumContentMationToDetails")
-//    public void queryForumContentMationToDetails(InputObject inputObject, OutputObject outputObject) {
-//        forumContentService.queryForumContentMationToDetails(inputObject, outputObject);
-//    }
-
     /**
      * 获取最新帖子
      *
      * @param inputObject  入参以及用户信息等获取对象
      * @param outputObject 出参以及提示信息的返回值对象
      */
-    /*@RequestMapping("/post/ForumContentController/queryNewForumContentList")
-    public void queryNewForumContentList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryNewForumContentList(inputObject, outputObject);
-    }*/
     @ApiOperation(id = "queryNewForumContentList", value = "获取最新帖子", method = "POST", allUse = "2")
     @RequestMapping("/post/ForumContentController/queryNewForumContentList")
     public void queryNewForumContentList(InputObject inputObject, OutputObject outputObject) {
         forumContentService.queryNewForumContentList(inputObject, outputObject);
     }
-//---------------------------------------------------------------------------------------------------
-    /**
-     * 新增帖子评论
-     *---------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/insertForumCommentMation")
-    public void insertForumCommentMation(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.insertForumCommentMation(inputObject, outputObject);
-    }
-
-    /**
-     * 获取帖子评论信息
-     *-----------------------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/queryForumCommentList")
-    public void queryForumCommentList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryForumCommentList(inputObject, outputObject);
-    }
-
-    /**
-     * 新增帖子评论回复
-     *----------------------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/insertForumReplyMation")
-    public void insertForumReplyMation(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.insertForumReplyMation(inputObject, outputObject);
-    }
-
-    /**
-     * 获取帖子评论回复信息
-     *------------------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/queryForumReplyList")
-    public void queryForumReplyList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryForumReplyList(inputObject, outputObject);
-    }
 
     /**
      * 获取我的浏览信息
-     *-----------------------
+     *
      * @param inputObject  入参以及用户信息等获取对象
      * @param outputObject 出参以及提示信息的返回值对象
      */
@@ -180,12 +104,12 @@ public class ForumContentController {
     }
 
     /**
-     * 获取最新评论
-     *------------------------
+     * 根据最新的评论获取前15条帖子
+     *
      * @param inputObject  入参以及用户信息等获取对象
      * @param outputObject 出参以及提示信息的返回值对象
      */
-    @ApiOperation(id = "queryNewCommentList", value = "获取最新评论", method = "POST", allUse = "2")
+    @ApiOperation(id = "queryNewCommentList", value = "根据最新的评论获取前15条帖子", method = "POST", allUse = "2")
     @RequestMapping("/post/ForumContentController/queryNewCommentList")
     public void queryNewCommentList(InputObject inputObject, OutputObject outputObject) {
         forumContentService.queryNewCommentList(inputObject, outputObject);
@@ -193,7 +117,7 @@ public class ForumContentController {
 
     /**
      * 根据标签id获取帖子列表
-     *------------------------
+     *
      * @param inputObject  入参以及用户信息等获取对象
      * @param outputObject 出参以及提示信息的返回值对象
      */
@@ -202,17 +126,6 @@ public class ForumContentController {
     @RequestMapping("/post/ForumContentController/queryForumListByTagId")
     public void queryForumListByTagId(InputObject inputObject, OutputObject outputObject) {
         forumContentService.queryForumListByTagId(inputObject, outputObject);
-    }
-
-    /**
-     * 获取热门标签
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/queryHotTagList")
-    public void queryHotTagList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryHotTagList(inputObject, outputObject);
     }
 
     /**
@@ -226,21 +139,6 @@ public class ForumContentController {
     public void queryActiveUsersList(InputObject inputObject, OutputObject outputObject) {
         forumContentService.queryActiveUsersList(inputObject, outputObject);
     }
-    /*@RequestMapping("/post/ForumContentController/queryActiveUsersList")
-    public void queryActiveUsersList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryActiveUsersList(inputObject, outputObject);
-    }*/
-
-    /**
-     * 获取热门贴
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/queryHotForumList")
-    public void queryHotForumList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryHotForumList(inputObject, outputObject);
-    }
 
     /**
      * 获取用户搜索的帖子
@@ -253,31 +151,9 @@ public class ForumContentController {
         forumContentService.querySearchForumList(inputObject, outputObject);
     }
 
-//    /**
-//     * 获取solr上次同步数据的时间
-//     *
-//     * @param inputObject  入参以及用户信息等获取对象
-//     * @param outputObject 出参以及提示信息的返回值对象
-//     */
-//    @RequestMapping("/post/ForumContentController/querySolrSynchronousTime")
-//    public void querySolrSynchronousTime(InputObject inputObject, OutputObject outputObject) {
-//        forumContentService.querySolrSynchronousTime(inputObject, outputObject);
-//    }
-
-//    /**
-//     * solr同步数据
-//     *
-//     * @param inputObject  入参以及用户信息等获取对象
-//     * @param outputObject 出参以及提示信息的返回值对象
-//     */
-//    @RequestMapping("/post/ForumContentController/updateSolrSynchronousData")
-//    public void updateSolrSynchronousData(InputObject inputObject, OutputObject outputObject) {
-//        forumContentService.updateSolrSynchronousData(inputObject, outputObject);
-//    }
-
     /**
      * 获取我的帖子列表
-     *-------------------
+     *
      * @param inputObject  入参以及用户信息等获取对象
      * @param outputObject 出参以及提示信息的返回值对象
      */
@@ -287,42 +163,4 @@ public class ForumContentController {
     public void queryMyCommentList(InputObject inputObject, OutputObject outputObject) {
         forumContentService.queryMyCommentList(inputObject, outputObject);
     }
-    /*@RequestMapping("/post/ForumContentController/queryMyCommentList")
-    public void queryMyCommentList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryMyCommentList(inputObject, outputObject);
-    }*/
-
-    /**
-     * 根据评论id删除评论
-     *--------------------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/deleteCommentById")
-    public void deleteCommentById(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.deleteCommentById(inputObject, outputObject);
-    }
-
-    /**
-     * 获取我的通知列表
-     *----------------------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/queryMyNoticeList")
-    public void queryMyNoticeList(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.queryMyNoticeList(inputObject, outputObject);
-    }
-
-    /**
-     * 根据通知id删除通知
-     *-----------------------
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
-    @RequestMapping("/post/ForumContentController/deleteNoticeById")
-    public void deleteNoticeById(InputObject inputObject, OutputObject outputObject) {
-        forumContentService.deleteNoticeById(inputObject, outputObject);
-    }
-
 }
