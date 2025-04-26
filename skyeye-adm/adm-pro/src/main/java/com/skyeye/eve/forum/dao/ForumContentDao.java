a4c7c1d401fae6d0a77463771a80b5ebffe2ab6f
weizhiqiang <598748873@qq.com>
2025-04-26 18:44:02 +0800

    feat:论坛管理

--- skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/dao/ForumContentDao.java
+++ skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/dao/ForumContentDao.java
@@ -7,9 +7,6 @@ package com.skyeye.eve.forum.dao;
 import com.skyeye.eve.dao.SkyeyeBaseMapper;
 import com.skyeye.eve.forum.entity.ForumContent;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * @ClassName: ForumContentDao
  * @Description: 论坛管理数据层
@@ -20,56 +17,5 @@ import java.util.Map;
  */
 public interface ForumContentDao extends SkyeyeBaseMapper<ForumContent> {
 
-    List<Map<String, Object>> queryMyForumContentList(Map<String, Object> map);
-
-    int insertForumContentMation(Map<String, Object> map);
-
-    int deleteForumContentById(Map<String, Object> map);
-
-    Map<String, Object> queryForumContentMationById(Map<String, Object> map);
-
-    int editForumContentMationById(Map<String, Object> map);
-
-    Map<String, Object> queryForumContentMationToDetails(Map<String, Object> map);
-
-    List<Map<String, Object>> queryNewForumContentList(Map<String, Object> map);
-
-    int insertForumCommentMation(Map<String, Object> map);
-
-    List<Map<String, Object>> queryForumCommentList(Map<String, Object> map);
-
-    int insertForumReplyMation(Map<String, Object> map);
-
-    List<Map<String, Object>> queryForumReplyList(Map<String, Object> map);
-
-    Map<String, Object> selectForumCommentNumById(Map<String, Object> map);
-
-    List<Map<String, Object>> queryNewCommentList(Map<String, Object> map);
-
-    List<Map<String, Object>> queryForumListByTagId(Map<String, Object> map);
-
-    List<Map<String, Object>> queryHotTagList(Map<String, Object> map);
-
-    List<Map<String, Object>> queryActiveUsersList(Map<String, Object> map);
-
-    List<Map<String, Object>> queryHotForumList(Map<String, Object> map);
-
-    List<Map<String, Object>> queryAllHotForumList(Map<String, Object> map);
-
-    List<Map<String, Object>> queryAllForumList(Map<String, Object> map);
-
-    List<Map<String, Object>> selectForumTagById(Map<String, Object> bean);
-
-    List<Map<String, Object>> queryMyCommentList(Map<String, Object> map);
-
-    int deleteCommentById(Map<String, Object> map);
-
-    List<Map<String, Object>> queryMyNoticeList(Map<String, Object> map);
-
-    int deleteNoticeById(Map<String, Object> map);
-
-    int insertForumHotByList(List<Map<String, Object>> list);
-
-    int insertForumStatisticsDayByList(List<Map<String, Object>> list);
 
 }
--- skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/dao/ForumTagDao.java
+++ skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/forum/dao/ForumTagDao.java
@@ -7,9 +7,6 @@ package com.skyeye.eve.forum.dao;
 import com.skyeye.eve.dao.SkyeyeBaseMapper;
 import com.skyeye.eve.forum.entity.ForumTag;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * @ClassName: ForumTagDao
  * @Description: 论坛标签管理数据层
@@ -20,33 +17,5 @@ import java.util.Map;
  */
 
 public interface ForumTagDao extends SkyeyeBaseMapper<ForumTag> {
-
-    List<Map<String, Object>> queryForumTagList(Map<String, Object> map);
-
-    Map<String, Object> queryForumTagMationByName(Map<String, Object> map);
-
-    int insertForumTagMation(Map<String, Object> map);
-
-    Map<String, Object> queryForumTagBySimpleLevel(Map<String, Object> map);
-
-    int deleteForumTagById(Map<String, Object> map);
-
-    int updateUpForumTagById(Map<String, Object> map);
-
-    int updateDownForumTagById(Map<String, Object> map);
-
-    Map<String, Object> selectForumTagById(Map<String, Object> map);
-
-    int editForumTagMationById(Map<String, Object> map);
-
-    Map<String, Object> queryForumTagUpMationById(Map<String, Object> map);
-
-    int editForumTagMationOrderNumUpById(Map<String, Object> map);
-
-    Map<String, Object> queryForumTagDownMationById(Map<String, Object> map);
-
-    Map<String, Object> queryForumTagStateById(Map<String, Object> map);
-
-    List<Map<String, Object>> queryForumTagUpStateList(Map<String, Object> map);
 
 }
