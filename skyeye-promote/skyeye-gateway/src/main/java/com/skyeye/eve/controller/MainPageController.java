978e8d299b287139037fb522e357be60cb194b09
weizhiqiang <598748873@qq.com>
2025-06-09 09:17:19 +0800

    feat： 添加控制层注解

--- skyeye-promote/skyeye-gateway/src/main/java/com/skyeye/eve/controller/MainPageController.java
+++ skyeye-promote/skyeye-gateway/src/main/java/com/skyeye/eve/controller/MainPageController.java
@@ -4,6 +4,7 @@
 
 package com.skyeye.eve.controller;
 
+import com.skyeye.annotation.api.Api;
 import com.skyeye.annotation.api.ApiOperation;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
@@ -13,17 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "首页统计相关", tags = "首页统计相关", modelName = "首页统计相关")
 public class MainPageController {
 
     @Autowired
     private MainPageService mainPageService;
 
-    /**
-     * 获取本月考勤天数，我的文件数，我的论坛帖数
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "mainpage001", value = "获取本月考勤天数，我的文件数，我的论坛帖数", method = "POST", allUse = "2")
     @RequestMapping("/post/MainPageController/queryFourNumListByUserId")
     public void queryFourNumListByUserId(InputObject inputObject, OutputObject outputObject) {
