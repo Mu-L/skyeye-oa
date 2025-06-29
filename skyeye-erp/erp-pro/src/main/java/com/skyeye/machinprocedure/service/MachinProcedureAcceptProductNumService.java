82c0e35155789dcd700c05ae4d8760796207a1fa
wst <2450473462@qq.com>
2025-06-29 19:17:59 +0800

    feat: erp工序验收增加员工生产数量

---
+++ skyeye-erp/erp-pro/src/main/java/com/skyeye/machinprocedure/service/MachinProcedureAcceptProductNumService.java
@@ -0,0 +1,14 @@
+package com.skyeye.machinprocedure.service;
+
+import com.skyeye.base.business.service.SkyeyeBusinessService;
+import com.skyeye.machinprocedure.entity.MachinProcedureAcceptProductNum;
+
+import java.util.List;
+
+public interface MachinProcedureAcceptProductNumService extends SkyeyeBusinessService<MachinProcedureAcceptProductNum> {
+    void writeList(String parentId, List<MachinProcedureAcceptProductNum> machinProcedureAcceptProductNumList);
+
+    List<MachinProcedureAcceptProductNum> queryListByParentId(String parentId);
+
+    void deleteByParentId(String parentId);
+}
