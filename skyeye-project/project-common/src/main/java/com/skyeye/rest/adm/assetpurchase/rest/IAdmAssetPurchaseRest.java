0f5f902907beb6291ce1155ba083d41f6364ac28
lqy <3431632001@qq,com>
2025-07-04 09:59:33 +0800

    feat:修改project项目管理成本核算定时任务配置有问题

--- skyeye-project/project-common/src/main/java/com/skyeye/rest/adm/assetpurchase/rest/IAdmAssetPurchaseRest.java
+++ skyeye-project/project-common/src/main/java/com/skyeye/rest/adm/assetpurchase/rest/IAdmAssetPurchaseRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "${webroot.skyeye-erp}", configuration = ClientConfiguration.class)
+@FeignClient(value = "${webroot.skyeye-adm}", configuration = ClientConfiguration.class)
 public interface IAdmAssetPurchaseRest {
 
     /**
