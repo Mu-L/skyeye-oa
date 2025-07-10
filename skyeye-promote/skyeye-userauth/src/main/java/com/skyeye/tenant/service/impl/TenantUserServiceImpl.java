a1daec34511c255a149c6e0da09048ec37e66a3f
DESKTOP-ESU4BDD\WeiZhiQiang <598748873@qq.com>
2025-07-10 09:07:28 +0800

    feat: 考勤班次设定修改

--- skyeye-promote/skyeye-userauth/src/main/java/com/skyeye/tenant/service/impl/TenantUserServiceImpl.java
+++ skyeye-promote/skyeye-userauth/src/main/java/com/skyeye/tenant/service/impl/TenantUserServiceImpl.java
@@ -160,7 +160,7 @@ public class TenantUserServiceImpl extends SkyeyeBusinessServiceImpl<TenantUserD
         tenantUser.setStaffMation(staffMap.get(tenantUser.getStaffId()));
 
         // 员工考勤时间段信息--一已适配多租户
-        List<Map<String, Object>> staffTimeMation = sysEveUserStaffTimeService.getStaffCheckWorkTimeByStaffId(id);
+        List<Map<String, Object>> staffTimeMation = sysEveUserStaffTimeService.getStaffCheckWorkTimeByStaffId(tenantUser.getStaffId());
         tenantUser.setTimeList(staffTimeMation);
 
         // 设置组织信息
