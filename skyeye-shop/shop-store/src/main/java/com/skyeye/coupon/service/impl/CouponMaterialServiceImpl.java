256c0b3fb7049364b5fe048c614bd0c83e4685d2
DESKTOP-ESU4BDD\WeiZhiQiang <598748873@qq.com>
2025-07-18 12:08:30 +0800

    feat: 商城适配多租户

--- skyeye-shop/shop-store/src/main/java/com/skyeye/coupon/service/impl/CouponMaterialServiceImpl.java
+++ skyeye-shop/shop-store/src/main/java/com/skyeye/coupon/service/impl/CouponMaterialServiceImpl.java
@@ -8,6 +8,7 @@ import cn.hutool.core.collection.CollectionUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.skyeye.annotation.service.SkyeyeService;
 import com.skyeye.base.business.service.impl.SkyeyeBusinessServiceImpl;
+import com.skyeye.common.enumeration.TenantEnum;
 import com.skyeye.common.util.mybatisplus.MybatisPlusUtil;
 import com.skyeye.coupon.dao.CouponMaterialDao;
 import com.skyeye.coupon.entity.CouponMaterial;
@@ -18,14 +19,14 @@ import java.util.List;
 
 /**
  * @ClassName: CouponMaterialServiceImpl
- * @Description: 优惠券/模版适用商品对象管理服务实现类
+ * @Description: 优惠券/模版适用商品对象管理服务实现类--不隔离
  * @author: skyeye云系列--卫志强
  * @date: 2024/10/23 10:37
  * @Copyright: 2024 https://gitee.com/doc_wei01/skyeye Inc. All rights reserved.
  * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
  */
 @Service
-@SkyeyeService(name = "优惠券/模版适用商品对象管理", groupName = "优惠券/模版适用商品对象管理", manageShow = false)
+@SkyeyeService(name = "优惠券/模版适用商品对象管理", groupName = "优惠券/模版适用商品对象管理", manageShow = false, tenant = TenantEnum.NO_ISOLATION)
 public class CouponMaterialServiceImpl extends SkyeyeBusinessServiceImpl<CouponMaterialDao, CouponMaterial> implements CouponMaterialService {
 
     @Override
