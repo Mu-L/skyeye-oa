3657dca181f9e20c7d4eacc062fd0402c31e778e
Chen <3307832075@qq.com>
2025-08-02 15:03:08 +0800

    feat：修改为听评表角色子表学时不为空

--- skyeye-school/school-pro/src/main/java/com/skyeye/school/lesson/entity/LessonReviewTypeChild.java
+++ skyeye-school/school-pro/src/main/java/com/skyeye/school/lesson/entity/LessonReviewTypeChild.java
@@ -1,14 +1,8 @@
 package com.skyeye.school.lesson.entity;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.skyeye.annotation.api.ApiModel;
-import com.skyeye.annotation.api.ApiModelProperty;
-import com.skyeye.common.entity.features.OperatorUserInfo;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 @TableName(value = "school_lesson_review_type")
