7a235e7731e03fbdc5d7f9ea70128379741af3d0
WeiZhiQiang <598748873@qq.com>
2026-03-21 21:24:14 +0800

    feat: 删除冗余注释

--- skyeye-erp/erp-pro/src/main/java/com/skyeye/product/controller/ProductReturnInStockController.java
+++ skyeye-erp/erp-pro/src/main/java/com/skyeye/product/controller/ProductReturnInStockController.java
@@ -8,10 +8,7 @@ import com.skyeye.common.entity.features.SubmitSkyeyeFlowable;
 import com.skyeye.common.entity.search.CommonPageInfo;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
-import com.skyeye.common.util.question.QuType;
-import com.skyeye.depot.entity.DepotOut;
 import com.skyeye.depot.entity.DepotPut;
-import com.skyeye.product.entity.ProductLeadOutStock;
 import com.skyeye.product.entity.ProductReturnInStock;
 import com.skyeye.product.service.ProductReturnInStockService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +22,6 @@ public class ProductReturnInStockController {
     @Autowired
     private ProductReturnInStockService productReturnInStockService;
 
-    /**
-     * 获取归还入库单列表
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "queryProductReturnInStockList", value = "获取归还入库单列表", method = "POST", allUse = "2")
     @ApiImplicitParams(classBean = CommonPageInfo.class)
     @RequestMapping("/post/ProductReturnInStockController/queryProductReturnInStockList")
@@ -38,12 +29,6 @@ public class ProductReturnInStockController {
         productReturnInStockService.queryPageList(inputObject, outputObject);
     }
 
-    /**
-     * 新增/编辑归还入库单
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "writeProductReturnInStock", value = "新增/编辑归还入库单", method = "POST", allUse = "2")
     @ApiImplicitParams(classBean = ProductReturnInStock.class)
     @RequestMapping("/post/ProductReturnInStockController/writeProductReturnInStock")
@@ -51,13 +36,6 @@ public class ProductReturnInStockController {
         productReturnInStockService.saveOrUpdateEntity(inputObject, outputObject);
     }
 
-
-    /**
-     * 转仓库入库单时，根据id查询归还入库单信息
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "queryProductReturnInStockById", value = "转仓库入库单时，根据id查询归还入库单信息", method = "GET", allUse = "2")
     @ApiImplicitParams({
         @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
@@ -66,12 +44,6 @@ public class ProductReturnInStockController {
         productReturnInStockService.selectById(inputObject, outputObject);
     }
 
-    /**
-     * 归还入库提交审批
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "submitProductReturnInStockToApproval", value = "归还入库提交审批", method = "POST", allUse = "2")
     @ApiImplicitParams(classBean = SubmitSkyeyeFlowable.class)
     @RequestMapping("/post/ProductReturnInStockController/submitProductReturnInStockToApproval")
@@ -79,12 +51,6 @@ public class ProductReturnInStockController {
         productReturnInStockService.submitToApproval(inputObject, outputObject);
     }
 
-    /**
-     * 删除归还入库
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "deleteProductReturnInStock", value = "删除归还入库", method = "DELETE", allUse = "2")
     @ApiImplicitParams({
         @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
@@ -93,12 +59,6 @@ public class ProductReturnInStockController {
         productReturnInStockService.deleteById(inputObject, outputObject);
     }
 
-    /**
-     * 撤销归还入库
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "revokeProductReturnInStock", value = "撤销归还入库", method = "PUT", allUse = "2")
     @ApiImplicitParams({
         @ApiImplicitParam(id = "processInstanceId", name = "processInstanceId", value = "流程实例id", required = "required")})
@@ -107,12 +67,6 @@ public class ProductReturnInStockController {
         productReturnInStockService.revoke(inputObject, outputObject);
     }
 
-    /**
-     * 归还入库单信息转仓库入库单
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "insertProductReturnInStockToInDepot", value = "归还入库单信息转仓库入库单", method = "POST", allUse = "2")
     @ApiImplicitParams(classBean = DepotPut.class, value = {
         @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
