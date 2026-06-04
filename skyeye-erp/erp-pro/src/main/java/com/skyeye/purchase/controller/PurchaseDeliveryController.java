d7c7cd913a112e674b1b2c4b9be2478a87deb3ba
WeiZhiQiang <598748873@qq.com>
2026-06-04 18:50:33 +0800

    feat: 删除多余注释

--- skyeye-erp/erp-pro/src/main/java/com/skyeye/purchase/controller/PurchaseDeliveryController.java
+++ skyeye-erp/erp-pro/src/main/java/com/skyeye/purchase/controller/PurchaseDeliveryController.java
@@ -13,7 +13,6 @@ import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
 import com.skyeye.inspection.entity.QualityInspection;
 import com.skyeye.purchase.entity.PurchaseDelivery;
-import com.skyeye.purchase.entity.PurchaseExchange;
 import com.skyeye.purchase.entity.PurchasePut;
 import com.skyeye.purchase.service.PurchaseDeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +34,6 @@ public class PurchaseDeliveryController {
     @Autowired
     private PurchaseDeliveryService purchaseDeliveryService;
 
-    /**
-     * 获取到货单列表
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "queryPurchaseDeliveryList", value = "获取到货单列表", method = "POST", allUse = "2")
     @ApiImplicitParams(classBean = CommonPageInfo.class)
     @RequestMapping("/post/PurchaseDeliveryController/queryPurchaseDeliveryList")
@@ -48,12 +41,6 @@ public class PurchaseDeliveryController {
         purchaseDeliveryService.queryPageList(inputObject, outputObject);
     }
 
-    /**
-     * 新增/编辑到货单
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "writePurchaseDelivery", value = "新增/编辑到货单", method = "POST", allUse = "1")
     @ApiImplicitParams(classBean = PurchaseDelivery.class)
     @RequestMapping("/post/PurchaseDeliveryController/writePurchaseDelivery")
@@ -61,57 +48,33 @@ public class PurchaseDeliveryController {
         purchaseDeliveryService.saveOrUpdateEntity(inputObject, outputObject);
     }
 
-    /**
-     * 转质检单时，根据id查询到货单信息
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "queryPurchaseDeliveryTransById", value = "转质检单时，根据id查询到货单信息", method = "GET", allUse = "2")
     @ApiImplicitParams({
-            @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
+        @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
     @RequestMapping("/post/PurchaseDeliveryController/queryPurchaseDeliveryTransById")
     public void queryPurchaseDeliveryTransById(InputObject inputObject, OutputObject outputObject) {
         purchaseDeliveryService.queryPurchaseDeliveryTransById(inputObject, outputObject);
     }
 
-    /**
-     * 到货单转质检单
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "deliveryToQualityInspection", value = "到货单转质检单", method = "POST", allUse = "1")
     @ApiImplicitParams(classBean = QualityInspection.class, value = {
-            @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
+        @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
     @RequestMapping("/post/PurchaseDeliveryController/deliveryToQualityInspection")
     public void deliveryToQualityInspection(InputObject inputObject, OutputObject outputObject) {
         purchaseDeliveryService.deliveryToQualityInspection(inputObject, outputObject);
     }
 
-    /**
-     * 转采购入库单时，根据id查询到货单信息
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "queryPurchaseDeliveryTransPurchasePutById", value = "转采购入库单时，根据id查询到货单信息", method = "GET", allUse = "2")
     @ApiImplicitParams({
-            @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
+        @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
     @RequestMapping("/post/PurchaseDeliveryController/queryPurchaseDeliveryTransPurchasePutById")
     public void queryPurchaseDeliveryTransPurchasePutById(InputObject inputObject, OutputObject outputObject) {
         purchaseDeliveryService.queryPurchaseDeliveryTransPurchasePutById(inputObject, outputObject);
     }
 
-    /**
-     * 到货单转采购入库单
-     *
-     * @param inputObject  入参以及用户信息等获取对象
-     * @param outputObject 出参以及提示信息的返回值对象
-     */
     @ApiOperation(id = "deliveryToPurchasePut", value = "到货单转采购入库单", method = "POST", allUse = "1")
     @ApiImplicitParams(classBean = PurchasePut.class, value = {
-            @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
+        @ApiImplicitParam(id = "id", name = "id", value = "主键id", required = "required")})
     @RequestMapping("/post/PurchaseDeliveryController/deliveryToPurchasePut")
     public void deliveryToPurchasePut(InputObject inputObject, OutputObject outputObject) {
         purchaseDeliveryService.deliveryToPurchasePut(inputObject, outputObject);
